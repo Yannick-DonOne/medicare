42f918e6240dc27d69b9e399af49feae572e7a27
Yannick-DonOne <njumeyannickdon1@gmail.com>
2021-11-18 17:29:55 +0100

    onboarding screen ui

--- lib/screens/general/onboarding/onboarding_screen.dart
+++ lib/screens/general/onboarding/onboarding_screen.dart
@@ -1,17 +1,271 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart' as flutter_svg;
+import 'package:medicare/utils/theme/theme.dart';
 
-class OnboardingScreen extends StatelessWidget {
-  static String id = "onboarding_screen";
-  const OnboardingScreen({Key? key}) : super(key: key);
+const List<String> assetNames = <String>[
+  'assets/images/onboard1.svg',
+  'assets/images/onboard2.svg',
+  'assets/images/onboard3.svg'
+];
+
+class OnboardingScreen extends StatefulWidget {
+  static String id = 'onboarding_screen';
+  @override
+  _OnboardingScreenState createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final int _numPages = 3;
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < _numPages; i++) {
+      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
+    }
+    return list;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 8.0,
+      width: isActive ? 24.0 : 16.0,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.white : Color(0xFF7B51D3),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text('Onboarding Screen'),
+      body: SafeArea(
+        child: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light,
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.1, 0.4, 0.7, 0.9],
+                colors: [
+                  Color(0xFF3594DD),
+                  Color(0xFF4563DB),
+                  Color(0xFF5036D5),
+                  Color(0xFF5B16D0),
+                ],
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    onPressed: () => print('Skip'),
+                    child: Text(
+                      'Skip',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: size.height * (3 / 4),
+                  child: PageView(
+                    physics: ClampingScrollPhysics(),
+                    controller: _pageController,
+                    onPageChanged: (int page) {
+                      setState(() {
+                        _currentPage = page;
+                      });
+                    },
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Center(
+                              child: flutter_svg.SvgPicture.asset(
+                                assetNames[0],
+                                width: size.width - 20,
+                                height: 200,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Text(
+                              'CONSULT',
+                              style: TextStyle(
+                                color: whiteColor,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 30,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: size.height * 0.05),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                                height: 1.2,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Center(
+                              child: flutter_svg.SvgPicture.asset(
+                                assetNames[2],
+                                width: size.width - 20,
+                                height: 200,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Text(
+                              'CONSULT',
+                              style: TextStyle(
+                                color: whiteColor,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 30,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: size.height * 0.05),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                                height: 1.2,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Center(
+                              child: flutter_svg.SvgPicture.asset(
+                                assetNames[1],
+                                width: size.width - 20,
+                                height: 200,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Text(
+                              'CONSULT',
+                              style: TextStyle(
+                                color: whiteColor,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 30,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: size.height * 0.05),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                                height: 1.2,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _buildPageIndicator(),
+                ),
+                _currentPage != _numPages - 1
+                    ? Expanded(
+                        child: Align(
+                          alignment: FractionalOffset.bottomRight,
+                          child: FlatButton(
+                            onPressed: () {
+                              _pageController.nextPage(
+                                duration: Duration(milliseconds: 500),
+                                curve: Curves.ease,
+                              );
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(
+                                  'Next',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.white,
+                                  size: 30.0,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    : Text(''),
+              ],
+            ),
+          ),
         ),
       ),
+      bottomSheet: _currentPage == _numPages - 1
+          ? Container(
+              height: size.height * 0.15,
+              width: double.infinity,
+              color: Colors.white,
+              child: GestureDetector(
+                onTap: () => print('Get started'),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      'Get started',
+                      style: TextStyle(
+                        color: Color(0xFF5B16D0),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : Text(''),
     );
   }
 }
