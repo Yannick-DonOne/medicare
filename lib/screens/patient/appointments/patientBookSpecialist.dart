8a981fb15e659ccddad6f4abc3abad66de502caf
Yannick-DonOne <njumeyannickdon1@gmail.com>
2021-11-27 08:38:57 +0100

    finished FAB

--- lib/screens/patient/appointments/patientBookSpecialist.dart
+++ lib/screens/patient/appointments/patientBookSpecialist.dart
@@ -1,5 +1,6 @@
 import 'package:dropdown_below/dropdown_below.dart';
 import 'package:flutter/material.dart';
+import 'package:medicare/screens/patient/find_doctor/doctor_list.dart';
 import 'package:medicare/screens/patient/find_doctor/text.dart';
 import 'package:medicare/utils/theme/theme.dart';
 
@@ -32,60 +33,106 @@ class _SelectSpecialistState extends State<SelectSpecialist> {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Specialist'),
+      ),
       body: SafeArea(
         child: Container(
-          child: Card(
-            child: Column(
-              children: [
-                Text(
-                  'Select Specialist',
-                  style: TextStyle(
-                      fontSize: size.width * 0.1, color: primaryColor),
-                ),
-                ValueListenableBuilder<String>(
-                  valueListenable: currentSpecialist,
-                  builder: (BuildContext context, String value, Widget? child) {
-                    return DropdownBelow(
-                      value: value,
-                      itemWidth: size.width,
-                      itemTextstyle: TextStyle(
-                        color: black.withOpacity(0.6),
-                        fontSize: size.width * 0.055,
-                        fontWeight: FontWeight.w700,
+          padding: EdgeInsets.all(8.0),
+          child: Center(
+            child: Card(
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Select Specialist',
+                      style: TextStyle(
+                        fontSize: size.width * 0.08,
+                        color: primaryColor,
+                        fontWeight: FontWeight.w500,
                       ),
-                      hint: Text(specialistList[0]),
-                      boxTextstyle: TextStyle(
-                        color: black.withOpacity(0.6),
-                        fontSize: size.width * 0.055,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      boxDecoration: BoxDecoration(
-                        color: whiteColor,
-                        border: Border.all(
-                          color: grey.withOpacity(0.8),
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(4),
-                        ),
-                      ),
-                      boxPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                      icon: Icon(
-                        Icons.keyboard_arrow_down,
-                        color: black.withOpacity(0.6),
-                        size: size.width * 0.13,
-                      ),
-                      boxHeight: size.height * 0.09,
-                      boxWidth: size.width,
-                      items: _dropdownTestItems =
-                          buildDropdownTestItems(specialistList),
-                      onChanged: (value) {
-                        currentSpecialist.value = value.toString();
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    ValueListenableBuilder<String>(
+                      valueListenable: currentSpecialist,
+                      builder:
+                          (BuildContext context, String value, Widget? child) {
+                        return DropdownBelow(
+                          value: value,
+                          itemWidth: size.width,
+                          itemTextstyle: TextStyle(
+                            color: black.withOpacity(0.6),
+                            fontSize: size.width * 0.055,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          hint: Text(specialistList[0]),
+                          boxTextstyle: TextStyle(
+                            color: black.withOpacity(0.6),
+                            fontSize: size.width * 0.055,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          boxDecoration: BoxDecoration(
+                            color: whiteColor,
+                            border: Border.all(
+                              color: grey.withOpacity(0.8),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(4),
+                            ),
+                          ),
+                          boxPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                          icon: Icon(
+                            Icons.keyboard_arrow_down,
+                            color: black.withOpacity(0.6),
+                            size: size.width * 0.13,
+                          ),
+                          boxHeight: size.height * 0.09,
+                          boxWidth: size.width,
+                          items: _dropdownTestItems =
+                              buildDropdownTestItems(specialistList),
+                          onChanged: (value) {
+                            currentSpecialist.value = value.toString();
+                          },
+                        );
                       },
-                    );
-                  },
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(context, DoctorList.id);
+                      },
+                      child: Container(
+                        height: 50,
+                        width: size.width * 0.5,
+                        decoration: BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Search',
+                            style: TextStyle(
+                              fontSize: size.width * 0.06,
+                              fontWeight: FontWeight.w500,
+                              color: whiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
